1f7c0ecc34052562c41d5f9a1acc5e7a44bd9b2a
LeTitouan <titouan.cazeneuve@gmail.com>
2019-05-20 15:15:26 -0600

    rollback

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_201227) do
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2019_05_20_174143) do
 
   create_table "comments", force: :cascade do |t|
     t.text "message", null: false
