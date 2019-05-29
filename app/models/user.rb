f64631025b7abb95395bf2b386dc8e63b8b08b0b
LeTitouan <titouan.cazeneuve@gmail.com>
2019-05-29 17:07:01 -0600

    Clean code

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@
 
 class User < ApplicationRecord
 
-  has_many :posts, dependent: :restrict_with_exception
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :trackable,
          :recoverable, :rememberable, :lockable
+
+  has_many :posts, dependent: :destroy
 
   validates :username, presence: true, length: { maximum: 50 }, uniqueness: true, format: { with: /[a-zA-Z]/ }
   validates :email, presence: true
